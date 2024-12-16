79f7d3166a720b2a6371dddd2b345bbec9720161
kimts2627 <like.gecko1@gmail.com>
2024-12-17 00:52:18 +0900

    feat: Footer 레이아웃 추가
    - react-use 패키지 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Appbar from "@/components/Appbar";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Daily JLPT",
@@ -15,12 +17,14 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className="antialiased">
-        <div className="flex justify-center h-screen">
-          <main className="flex flex-col h-full border w-full max-w-[900px] px-4">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <main className="flex flex-col h-full w-full max-w-[900px] px-4">
             <Appbar />
             {children}
           </main>
+          <Footer />
         </div>
+        <Toaster />
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useToast } from "@/hooks/use-toast";
+import { useCopyToClipboard } from "react-use";
+
+const EMAIL = "like.gecko1@gmail.com";
+
+const Footer = () => {
+  const { toast } = useToast();
+  const [state, copy] = useCopyToClipboard();
+
+  const copyEmail = () => {
+    copy(EMAIL);
+    toast({
+      title: "이메일이 복사 되었습니다.",
+    });
+  };
+
+  return (
+    <footer className="flex items-center justify-center h-[150px] w-full bg-gray-200">
+      <div className="h-full flex flex-col w-full max-w-[900px] p-4 sm:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <span className="text-sm text-gray-500">
+            Copyright © 2025, Daily JLPT. All rights reserved.
+          </span>
+          <span
+            className="text-sm text-gray-500 font-bold cursor-pointer"
+            onClick={copyEmail}
+          >
+            {EMAIL}
+          </span>
+        </div>
+        <div className="text-gray-500 text-sm">이것저것</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
